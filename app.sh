4a4f2cd5968f2007206ce3ecd4de785aadce034d
ashiwaf <atsushi.ashiwa@flect.co.jp>
2020-11-19 14:46:07 +0900

    add: Coolterm

--- app.sh
+++ app.sh
@@ -50,6 +50,9 @@ brew install nodebrew
 brew cask install toggl-track 
 brew cask install thunderbird
 
+# For Work
+brew cask install coolterm
+
 # AWS-CLI
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "/tmp/AWSCLIV2.pkg"
 sudo installer -pkg /tmp/AWSCLIV2.pkg -target /

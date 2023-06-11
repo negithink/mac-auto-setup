0feeb2f0f2c2201459698cef1a843d69fdc04f34
negithink <ashiwa.account@gmail.com>
2023-06-11 11:09:45 +0900

    modify: app stores apps

--- appstore.sh
+++ appstore.sh
@@ -13,19 +13,7 @@ EOS
 #
 echo " ---- Mac App Store apps -----"
 brew install mas
-# mas install 1144071713 # KeiSignal (1.0.3)
-# mas install 409183694  # Keynote (6.6.2)
 mas install 425424353  # The Unarchiver (3.11.1)
-# mas install 568494494  # Pocket (1.6.2)
-# mas install 524688159  # Desktop Calendar Plus (1.3.6)
-# mas install 409201541  # Pages (5.6.2)
-# mas install 1024640650 # CotEditor (3.1.2)
-# mas install 453164367  # SystemPal (5.2)
-# mas install 412529613  # Cinch (1.2.2)
-# mas install 409203825  # Numbers (3.6.2)
-# mas install 1054607607 # Helium (2.0)
-# mas install 476533227  # Prepo (2.2.7)
-# mas install 414298354  # ToyViewer (5.5.1)
 mas install 549083868 #  # Display Menu (2.2.3)
 mas install 539883307 # LINE (6.3.2)
 mas install 425264550 # Disk Speed Test (3.2)
@@ -41,4 +29,11 @@ mas install 736189492 # Notability (4.4.1)
 mas install 409201541 # Pages (7.3)
 mas install 1295203466 # Microsoft Remote Desktop (10.4.1)
 mas install 803453959 # Slack (4.10.3)
+mas install 747648890 # Telegram (8.3)
+mas install 1295203466 # Microsoft Remote Desktop (10.7.3)
+mas install 957734279 # Toggl Track (7.5.441)
+mas install 946798523 # Sleep Control Center (2.19)
+mas install 549083868 # Display Menu (2.2.3)
+mas install 1548099536  # WattageViewer (1.3.2)
+
 echo " ------------ END ------------"

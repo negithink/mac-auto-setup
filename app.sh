90bdf6e082e6947990b6dcce6b66331c3d53be8e
negithink <ashiwa.account@gmail.com>
2024-11-05 17:25:15 +0900

    Remove: 不要なものをお掃除

--- app.sh
+++ app.sh
@@ -13,72 +13,55 @@ EOS
 #
 echo " ----- Install web apps ------"
 # brew install --cask 1password
-brew install --cask android-file-transfer
+# brew install --cask android-file-transfer
 brew install --cask java
-# brew install --cask android-studio
 # brew install --cask arduino
-brew install --cask astropad
-# brew install --cask atom
-# brew install --cask cheatsheet
 brew install --cask cyberduck
 brew install --cask docker
-# brew install --cask dropbox
 # brew install --cask duet
 # brew install --cask eclipse-java
 brew install --cask google-chrome
 brew install --cask iterm2
 # brew install --cask sourcetree
-brew install --cask virtualbox
-brew install --cask virtualbox-extension-pack
 brew install --cask vlc
 brew install --cask google-japanese-ime
 brew install --cask swimat
-# brew install --cask spotify
-# brew install --cask imageoptim
-# brew install --cask soundflower
 brew install --cask visual-studio-code
-# brew install --cask adobe-creative-cloud
 # brew install --cask gyazo
 brew install --cask zoomus
 # brew install --cask tandem
 brew install --cask discord
-brew install --cask scroll-reverser  # 動作未確認
-brew tap homebrew/cask-drivers       # 動作未確認
-brew install logitech-control-center # 動作未確認
+brew install --cask scroll-reverser
+brew tap homebrew/cask-drivers
 brew install pyenv
 brew install nodebrew
 brew install tree
 brew install --cask monitorcontrol
 brew install --cask toggl-track
-brew install --cask thunderbird
 brew install --cask adobe-acrobat-reader
 brew install --cask tableplus
 npm install --global git-open
 brew install --cask drawio
 brew install git-remote-codecommit
 npm install -g awsp
-brew install --cask mysqlworkbench
 brew install --cask session-manager-plugin
 brew install --cask clipy
-brew install --cask lg-onscreen-control
 brew install imagemagick
 brew install peco # zsh
 brew install --cask kindle
 brew install --cask omnidisksweeper
 
 # For Work
-brew install --cask coolterm
 brew install mysql
-brew install sops
+# brew install sops
 brew install --cask postman
-brew install --cask sfdx
+# brew install --cask sfdx
 brew tap heroku/brew
-brew install heroku
+# brew install heroku
 brew install postgresql
 brew install --cask dbeaver-community
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
-brew install phpbrew
 brew install jq
 brew install gh
 gh ext install meiji163/gh-notify
@@ -88,13 +71,10 @@ brew install pass
 brew install dmenu
 brew install act
 brew install actionlint
-brew install php-cs-fixer
-brew install librsvg
+# brew install php-cs-fixer
+# brew install librsvg
 brew install plantuml
 brew install source-highlight
-
-# For Rust/cargo
-curl https://sh.rustup.rs -sSf | sh
 
 # For CFn
 brew install cfn-format
